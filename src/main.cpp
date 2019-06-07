1f21babfddad3da11f537ebc909ba07050317957
DavidKeaton <keaton.david@gmail.com>
2019-06-07 13:48:58 -0400

    root dir cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 
 int main()
 {
+	// XXX: TESTING LOGGER AND COMPILATION
 	Logger *Log = new Logger();
 	Log->d("TEST", "testing %s", "things");
+
 	std::cout << "Hello, World!" << std::endl;
+
 	delete Log;
 	return 0;
 }
